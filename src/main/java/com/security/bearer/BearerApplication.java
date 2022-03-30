454af0da76691a3b8a4efdd361c50d396d292547
Marcos Vinicius <viniciushkd@gmail.com>
2022-03-30 14:50:38 -0300

    Commit Test

--- src/main/java/com/security/bearer/BearerApplication.java
+++ src/main/java/com/security/bearer/BearerApplication.java
@@ -3,6 +3,7 @@ package com.security.bearer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Test
 @SpringBootApplication
 public class BearerApplication {
 

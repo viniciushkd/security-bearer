bcd0b70af44494719c224249fff0c06bd5de9e95
Marcos Vinicius <viniciushkd@gmail.com>
2022-04-14 11:17:25 -0300

    Bugfix User. #12

--- src/main/java/com/security/bearer/business/UserBusiness.java
+++ src/main/java/com/security/bearer/business/UserBusiness.java
@@ -14,15 +14,16 @@ import com.security.bearer.dto.UserRDTO;
 import com.security.bearer.repository.RoleRepository;
 import com.security.bearer.repository.UserRepository;
 import com.security.bearer.repository.data.ERole;
+import com.security.bearer.repository.data.Role;
 import com.security.bearer.repository.data.User;
 
 @Component
 public class UserBusiness {
-	
+
 	private UserRepository userRepository;
 	private RoleRepository roleRepository;
 	private Encrypt encrypt;
-	
+
 	public UserBusiness(UserRepository userRepository, RoleRepository roleRepository, Encrypt encrypt) {
 		super();
 		this.userRepository = userRepository;
@@ -31,19 +32,28 @@ public class UserBusiness {
 	}
 
 	public void saveUser(String user, String password) {
-		this.userRepository.save(new User(user, this.encrypt.md5(password), UUID.randomUUID().toString(), 
-				Arrays.asList(this.roleRepository.findByName(ERole.ROLE_USER).get())));
+		Optional<Role> role = this.roleRepository.findByName(ERole.ROLE_USER);
+		if (role.isPresent()) {
+			this.userRepository.save(new User(user, this.encrypt.md5(password), UUID.randomUUID().toString(),
+					Arrays.asList(role.get())));
+		}
 	}
 
 	public UserRDTO getUser(String uidUser) {
 		final Optional<User> optional = userRepository.findByUid(uidUser);
-		return optional.map(user -> new UserRDTO(user.getUser(), user.getUid(), user.getRoles().stream()
-				.map(roles -> new RoleDTO(roles.getName())).collect(Collectors.toList()))).get();
+		if (optional.isPresent()) {
+			return optional.map(user -> new UserRDTO(user.getUser(), user.getUid(),
+					user.getRoles().stream().map(roles -> new RoleDTO(roles.getName())).collect(Collectors.toList()))).get();
+		} else {
+			return new UserRDTO("", "", null);
+		}
 	}
 
 	public List<UserRDTO> listUser() {
 		final List<User> user = userRepository.findAll();
-		return user.stream().map(usr -> new UserRDTO(usr.getUser(), usr.getUid(), usr.getRoles().stream()
-				.map(roles -> new RoleDTO(roles.getName())).collect(Collectors.toList()))).collect(Collectors.toList());
+		return user.stream()
+				.map(usr -> new UserRDTO(usr.getUser(), usr.getUid(), usr.getRoles().stream()
+						.map(roles -> new RoleDTO(roles.getName())).collect(Collectors.toList())))
+				.collect(Collectors.toList());
 	}
 }
